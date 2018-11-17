cdb3539e1bf1bd951268906b78b6bb224083f3af
Rafael Coutinho <rafacout@gmail.com>
2018-11-17 21:07:35 -0200

    Adjusts in projects, Clientes model and enabled migrations

---
+++ SalesManagement/SalesManagement.Domain/Entities/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement.Domain.Entities
+{
+    public class Cliente
+    {
+
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+        }
+    }
+}
---
+++ SalesManagement/SalesManagement.Infra.Data/Context/SalesManagementContext.cs
@@ -0,0 +1,16 @@
+﻿using SalesManagement.Domain.Entities;
+using System.Data.Entity;
+
+namespace SalesManagement.Infra.Data.Context
+{
+    public class SalesManagementContext : DbContext
+    {
+        public SalesManagementContext() : base("SalesManagement")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
