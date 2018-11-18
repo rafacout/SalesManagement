7ab584e020c2cc1474739f393416d787c94f315b
Rafael Coutinho <rafacout@gmail.com>
2018-11-18 18:32:55 -0200

    Included ProdutoAppService

---
+++ SalesManagement/SalesManagement.Application/ProdutoAppService.cs
@@ -0,0 +1,27 @@
+﻿using SalesManagement.Application.Interfaces;
+using SalesManagement.Domain.Entities;
+using SalesManagement.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement.Application
+{
+    public class ProdutoAppService : AppServiceBase<Produto>, IProdutoAppService
+    {
+        private readonly IProdutoService _produtoService;
+
+        public ProdutoAppService(IProdutoService produtoService)
+            : base(produtoService)
+        {
+            _produtoService = produtoService;
+        }
+
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return _produtoService.BuscarPorNome(nome);
+        }
+    }
+}
