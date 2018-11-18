f5cbde4d9f922b6ffcbdc6857290a9ef43dd1d90
Rafael Coutinho <rafacout@gmail.com>
2018-11-18 19:03:18 -0200

    Included Ninject

---
+++ SalesManagement/SalesManagement.MVC/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+using SalesManagement.Application;
+using SalesManagement.Application.Interfaces;
+using SalesManagement.Domain.Interfaces.Repositories;
+using SalesManagement.Domain.Interfaces.Services;
+using SalesManagement.Domain.Services;
+using SalesManagement.Infra.Data.Repositories;
+using SalesManagement.MVC;
+using System;
+using System.Web;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace SalesManagement.MVC
+{
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+            return kernel;
+        }
+        private static void RegisterServices(IKernel kernel)
+        {
+            //kernel.Bind<IRepo>().ToMethod(ctx => new Repo("Ninject Rocks!"));
+            kernel.Bind(typeof(IAppServiceBase<>)).To(typeof(AppServiceBase<>));
+            kernel.Bind<IClienteAppService>().To<ClienteAppService>();
+            kernel.Bind<IProdutoAppService>().To<ProdutoAppService>();
+
+            kernel.Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
+            kernel.Bind<IClienteService>().To<ClienteService>();
+            kernel.Bind<IProdutoService>().To<ProdutoService>();
+
+            kernel.Bind(typeof(IRepositoryBase<>)).To(typeof(RepositoryBase<>));
+            kernel.Bind<IClienteRepository>().To<ClienteRepository>();
+            kernel.Bind<IProdutoRepository>().To<ProdutoRepository>();
+        }
+    }
+}
