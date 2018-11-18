6b1c1054205c7e929cec2af2e62d6c8ef0a970c5
Rafael Coutinho <rafacout@gmail.com>
2018-11-18 14:47:45 -0200

    Added support to AutoMapper

---
+++ SalesManagement/SalesManagement.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SalesManagement.Domain.Entities;
+using SalesManagement.MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesManagement.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<ProdutoViewModel, Produto>();
+        }
+    }
+}
---
+++ SalesManagement/SalesManagement.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SalesManagement.Domain.Entities;
+using SalesManagement.MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesManagement.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Cliente, ClienteViewModel>();
+            Mapper.CreateMap<Produto, ProdutoViewModel>();
+        }
+    }
+}
